ae683c23db0af3f85c2a4f4e02936edbb0025c7b
Manmeet Singh <manmeets.zsh@gmail.com>
2023-08-24 18:10:43 +0530

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,33 @@
+import { VitePWA } from 'vite-plugin-pwa';
+export default {
+  plugins: [
+    VitePWA({
+      // generates 'manifest.webmanifest' file on build
+      manifest: {
+        // caches the assets/icons mentioned (assets/* includes all the assets present in your src/ directory) 
+        includeAssets: ["favicon.ico", "apple-touch-icon.png", "assets/*"],
+        name: 'Simplifying Progressive Web App (PWA) Development with Vite: A Beginners Guide',
+        short_name: 'PWA Guide',
+        start_url: '/',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: '/images/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/images/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+      workbox: {
+        // defining cached files formats
+        globPatterns: ["**/*.{js,css,html,ico,png,svg,webmanifest}"],
+      }
+    })
+  ]
+};
