cbdc648b8cfc41ed2dabe7959379750ce63b1958
Rohit Mehta <63762981+r0hitm@users.noreply.github.com>
2023-08-26 11:32:06 +0530

    corrected icons path; haven't tested

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,26 @@
-import { VitePWA } from 'vite-plugin-pwa';
+import { VitePWA } from "vite-plugin-pwa";
 export default {
-  plugins: [
-    VitePWA({
-      // generates 'manifest.webmanifest' file on build
-      manifest: {
-        // caches the assets/icons mentioned (assets/* includes all the assets present in your src/ directory) 
-        includeAssets: ["favicon.ico", "apple-touch-icon.png", "assets/*"],
-        name: 'Time Table - 5th Semester',
-        short_name: 'Time Table',
-        start_url: '/',
-        background_color: '#111827',
-        theme_color: '#111827',
-        icons: [
-          {
-            src: '/images/icon-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: '/images/icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      },
-      workbox: {
-        // defining cached files formats
-        globPatterns: ["**/*.{js,css,html,ico,png,svg,webmanifest}"],
-      }
-    })
-  ]
+    plugins: [
+        VitePWA({
+            // generates 'manifest.webmanifest' file on build
+            manifest: {
+                name: "Time Table - 5th Semester",
+                short_name: "Time Table",
+                start_url: "/",
+                background_color: "#111827",
+                theme_color: "#111827",
+                icons: [
+                    {
+                        src: "/public/favicon.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                    },
+                ],
+            },
+            workbox: {
+                // defining cached files formats
+                globPatterns: ["**/*.{js,css,html,ico,png,svg,webmanifest}"],
+            },
+        }),
+    ],
 };
