e958f4e2e69b89f5697bc5ddd012e6cf5ecc2316
Rohit Mehta <63762981+r0hitm@users.noreply.github.com>
2023-08-26 11:39:47 +0530

    Revert "redundant config"
    This reverts commit 963735032c53eccb8cd69213d8a7638975f1fc40.

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,26 @@
 import { VitePWA } from "vite-plugin-pwa";
 export default {
-    plugins: [VitePWA()],
+    plugins: [
+        VitePWA({
+            // generates 'manifest.webmanifest' file on build
+            manifest: {
+                name: "Time Table - 5th Semester",
+                short_name: "Time Table",
+                start_url: "/",
+                background_color: "#111827",
+                theme_color: "#111827",
+                icons: [
+                    {
+                        src: "/public/favicon.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                    },
+                ],
+            },
+            workbox: {
+                // defining cached files formats
+                globPatterns: ["**/*.{js,css,html,ico,png,svg,webmanifest}"],
+            },
+        }),
+    ],
 };
